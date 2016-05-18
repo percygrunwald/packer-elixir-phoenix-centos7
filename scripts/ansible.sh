c1651c873cbd89e05976eec388189d185f9a0c2f
Percy Grunwald <percy.grunwald@gmail.com>
2016-05-18 16:30:45 +0800

    Updates to ansible from geerlingguy role

--- scripts/ansible.sh
+++ scripts/ansible.sh
@@ -4,4 +4,7 @@
 yum -y install epel-release
 
 # Install Ansible.
-yum -y install ansible python-setuptools
+yum -y install git python-setuptools gcc sudo libffi-devel python-devel openssl-devel
+yum clean all
+easy_install pip
+pip install ansible
